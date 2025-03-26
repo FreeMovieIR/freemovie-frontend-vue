be638e593ee49b4e7b5da17fa28cbf0029468b44
sohseyedi <sohseyedi@gmail.com>
2025-03-26 22:19:40 +0330

    feat: create Navbar with navigation links and search input

---
+++ src/components/common/Navbar.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import InputSearch from '@/ui/InputSearch.vue'
+</script>
+
+<template>
+  <nav class="flex items-center justify-between">
+    <div class="flex items-center gap-x-5">
+      <router-link to="/" class="text-xs">
+        خانه
+      </router-link>
+      <router-link to="/" class="text-xs">
+        واچ لیست
+      </router-link>
+      <router-link to="/" class="text-xs">
+        توسعه دهندگان
+      </router-link>
+      <router-link to="/" class="text-xs">
+        درباره ما
+      </router-link>
+    </div>
+    <InputSearch />
+    <img src="/logo-free-movie.png" alt="free-movie" class="w-[78px] h-[29px] object-cover">
+  </nav>
+</template>
