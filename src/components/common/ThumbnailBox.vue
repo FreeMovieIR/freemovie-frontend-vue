322cd63d060c4a04bb580cae256861c8acb6654c
sohseyedi <sohseyedi@gmail.com>
2025-03-26 22:21:48 +0330

    feat: create ThumbnailBox for displaying media with interactive elements

---
+++ src/components/common/ThumbnailBox.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :style="{ backgroundImage: `url(/pic-1.jpg)` }" class="bg-cover p-5 relative bg-center h-[446px] rounded-[.9rem] w-full">
+    <div class="absolute inset-0 bg-black/50" />
+    <div class="absolute bottom-4 right-4 space-y-4">
+      <h2 class="text-3xl text-white">
+        I’m Still Here
+      </h2>
+      <p class="text-sm text-[#CAD5E2] tracking-wide">
+        داستان مادری که پس از فروپاشی خانواده‌اش در یک حادثه طی دیکتاتوری نظامی در<br> سال 1971 و در برزیل، باید دوباره روی پای خودش بایستد تا...
+      </p>
+      <div class="flex items-center gap-x-3">
+        <button class="bg-[#BEDBFF] p-2 rounded-full text-[#020618] text-sm">
+          دانلود
+        </button>
+        <button class="bg-[#193CB8] p-2 rounded-full text-sm text-[#BEDBFF]">
+          واچ لیست
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
