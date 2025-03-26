df7637c499fb522349ed40b244789dd070bb7fdf
sohseyedi <sohseyedi@gmail.com>
2025-03-26 22:22:25 +0330

    feat: create HomeWrapper component

---
+++ src/components/home/HomeWrapper.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import AlertNotice from '../common/AlertNotice.vue'
+import Footer from '../common/Footer.vue'
+import Navbar from '../common/Navbar.vue'
+import ThumbnailBox from '../common/ThumbnailBox.vue'
+</script>
+
+<template>
+  <Navbar />
+  <AlertNotice />
+  <ThumbnailBox />
+  <hr class="my-5 border-[#1D293D] border">
+  <Footer />
+</template>
