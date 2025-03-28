60b578e28b5c2188094c03fe332e69b28468c4c8
sohseyedi <sohseyedi@gmail.com>
2025-03-28 13:55:40 +0330

    update route and build

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import { resolve } from 'node:path'
-import vue from '@vitejs/plugin-vue'
-import { defineConfig } from 'vite'
+import { resolve } from "node:path";
+import vue from "@vitejs/plugin-vue";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/freemovie-frontend-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
