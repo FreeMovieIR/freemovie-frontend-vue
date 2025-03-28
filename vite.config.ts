d40fd17d0f46cd7bc28f64f99502456b331ef8e9
sohseyedi <sohseyedi@gmail.com>
2025-03-28 13:40:06 +0330

    chore:update route and build

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,12 @@ import vue from '@vitejs/plugin-vue'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+  base: "/freemovie-frontend-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, './src'),
+      "@": resolve(__dirname, "./src"),
     },
-    mainFields: ['browser', 'module', 'main', 'jsnext:main', 'jsnext'],
+    mainFields: ["browser", "module", "main", "jsnext:main", "jsnext"],
   },
-})
+});
