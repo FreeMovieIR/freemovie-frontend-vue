41f309776d6d620547b547d2c7637cf907067562
sohseyedi <sohseyedi@gmail.com>
2025-03-28 14:31:59 +0330

    chore:add base url

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/freemovie-frontend-vue/",
+  base: '/freemovie-frontend-vue/',
   plugins: [vue()],
   resolve: {
     alias: {
